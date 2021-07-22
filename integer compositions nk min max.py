4b2d49c3d8983bfd30ba12591d7eac0f13edefba
Luan Simões <luansimoes@id.uff.br>
2021-07-22 18:30:33 -0300

    optimize generation

--- integer compositions nk min max.py
+++ integer compositions nk min max.py
@@ -1,18 +1,28 @@
-def constrained_compositions(n, k, min_elem, max_elem):
+def constrained_compositions(n, k, min_elem, max_elem, equiv=12):
     allowed = range(max_elem, min_elem-1, -1)
 
-    def helper(n, k, t):
+    def update_sum(s, v, t):
+        if len(t)==0:
+            return []
+        else:
+            return [(x+v)%equiv for x in s] + [(t[-1]+v) % equiv]
+
+    def helper(n, k, t, s):
         if k == 0:
             if n == 0:
                 yield t
-        elif k == 1:
-            if n in allowed:
-                yield t + (n,)
-        elif min_elem * k <= n <= max_elem * k:
-            for v in allowed:
-                yield from helper(n - v, k - 1, t + (v,))
+        
+        elif not 0 in s:
 
-    return helper(n, k, ())
+            if k == 1:
+                if n in allowed:
+                    yield t + (n,)
+        
+            elif min_elem * k <= n <= max_elem * k:
+                for v in allowed:
+                    yield from helper(n - v, k - 1, t + (v,), update_sum(s, v, t))
+
+    return helper(n, k, (), [])
 
 # Original
 '''
@@ -21,20 +31,4 @@ for p in constrained_compositions(12, 3, 3, 7):
 '''
 
 for p in constrained_compositions(72, 12, 1, 11):
-    n = 12
-    d = 2
-    k = 12
-    intervals = []
-    for i in range(k):
-        if i == 0:
-            for j in range(i, k-1):
-                summation = (sum(p[i:j+1])) % n
-                intervals.append(summation)
-            
-        else:
-            for j in range(i, k):
-                summation = (sum(p[i:j+1])) % n
-                intervals.append(summation)
-            
-    if (0 not in intervals) and (p.count(1) == 1):
-        print(p)
+    print(p)

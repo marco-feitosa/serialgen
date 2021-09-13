61e015aca07267c843b251100d0601577c9a70c2
Raphael Sousa Santos <raphael@sonologi.co>
2021-09-13 21:46:29 +0200

    update classify.py

--- classify.py
+++ classify.py
@@ -1,3 +1,5 @@
+import math
+from multiprocessing import Process, Queue
 import sys
 
 def inversion(series):
@@ -16,6 +18,18 @@ def retrograde_inversion(series):
 def quart_circle(series):
     return [(5 * x) % 12 for x in series]
 
+def five_step(series):
+    repeated_series = series + series
+    result = []
+    for i in range(len(series)):
+        start = (i * 5) % 12
+        result.append(sum(repeated_series[start:start+5]) % 12)
+    return result
+
+def append_transform(transform, forms):
+    for i in range(len(forms)):
+        forms.append(transform(forms[i]))
+
 def rotations(x):
     result = []
     for i in range(len(x)):
@@ -28,7 +42,7 @@ def with_rotations(forms):
 
 def lowest_by_snd(pairs):
     val = None
-    result = None 
+    result = None
     for x in pairs:
         if not val:
             val = x[1]
@@ -76,8 +90,20 @@ def tisr_normalization(series):
 
 def tiqsr_normalization(series):
     forms = [series, inversion(series), retrograde(series), retrograde_inversion(series)]
-    for i in range(len(forms)):
-        forms.append(quart_circle(forms[i]))
+    append_transform(quart_circle, forms)
+    forms = with_rotations(forms)
+    return most_compact(forms)
+
+def tisrf_normalization(series):
+    forms = [series, inversion(series), retrograde(series), retrograde_inversion(series)]
+    append_transform(five_step, forms)
+    forms = with_rotations(forms)
+    return most_compact(forms)
+
+def tiqsrf_normalization(series):
+    forms = [series, inversion(series), retrograde(series), retrograde_inversion(series)]
+    append_transform(quart_circle, forms)
+    append_transform(five_step, forms)
     forms = with_rotations(forms)
     return most_compact(forms)
 
@@ -93,25 +119,74 @@ methods = {
   "ts": ts_normalization,
   "tis": tis_normalization,
   "tisr": tisr_normalization,
-  "tiqsr": tiqsr_normalization
+  "tiqsr": tiqsr_normalization,
+  "tisrf": tisrf_normalization,
+  "tiqsrf": tiqsrf_normalization
 }
 
-def classify_from_file(method_name, file):
+def classify_from_lines(method_name, lines):
     method = methods[method_name]
     classes = {}
+    len_str = str(len(lines))
+    for i, line in enumerate(lines):
+        series = read_series(line)
+        if not series:
+            continue
+        normal_form = tuple(method(series))
+        if normal_form in classes:
+            classes[normal_form] += 1
+        else:
+            classes[normal_form] = 1
+    return classes
+
+def report_classes(classes):
+    for series, count in classes.items():
+        print(str(series), count)
+    print("\ntotal: ", len(classes))
+
+def merge(into, new):
+    for form, count in new.items():
+        if form in into:
+            into[form] = into[form] + count
+        else:
+            into[form] = count
+
+def child_process_classify(queue, method_name, lines):
+    result = classify_from_lines(method_name, lines)
+    queue.put(result)
+    exit(0)
+
+def create_process(queue, method_name, lines):
+    return Process(target=child_process_classify, args=(queue, method_name, lines))
+
+def parallel_classify_from_lines(method_name, lines, nr_processes):
+    queue = Queue()
+    batch_size = math.ceil(len(lines) / nr_processes)
+    processes = [create_process(queue, method_name, lines[i:i + batch_size]) for i in range(0, len(lines), batch_size)]
+    for process in processes:
+        process.start()
+    classes = {}
+    for process in processes:
+        process.join(1)
+        while process.is_alive():
+            process.join(1)
+            if not queue.empty():
+                merge(classes, queue.get())
+    while not queue.empty():
+        merge(classes, queue.get())
+    return classes
+
+def classify_from_file(method_name, file, nr_processes):
     with open(file, "rt") as fin:
-        for line in fin.readlines():
-            series = read_series(line)
-            if not series:
-                continue
-            normal_form = tuple(method(series))
-            if normal_form in classes:
-                classes[normal_form] += 1
-            else:
-                classes[normal_form] = 1
-        for series, count in classes.items():
-            print(str(series), count)
-        print("\ntotal: ", len(classes))
+        lines = fin.readlines()
+    if nr_processes == 1:
+        classes = classify_from_lines(method_name, lines)
+    else:
+        classes = parallel_classify_from_lines(method_name, lines, nr_processes)
+    report_classes(classes)
 
 if __name__ == '__main__':
-    classify_from_file(sys.argv[1], sys.argv[2])
+    nr_processes = 1
+    if len(sys.argv) > 3:
+        nr_processes = int(sys.argv[3])
+    classify_from_file(sys.argv[1], sys.argv[2], nr_processes)
